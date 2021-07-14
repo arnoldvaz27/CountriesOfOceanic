5e86b8e3a17293067f426d537eaab17c17b29a45
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-07-14 20:47:40 +0530

    Countries of Oceanic project added to git

---
+++ app/src/main/java/com/arnold/countriesofoceanic/listeners/CountryListeners.java
@@ -0,0 +1,9 @@
+package com.arnold.countriesofoceanic.listeners;
+
+import com.arnold.countriesofoceanic.entites.Country;
+
+public interface CountryListeners {
+
+    void onItemClicked(Country country, int position); //event listener after clicking on the view
+
+}
